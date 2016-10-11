7e0b5b9c5ca1ed43744b31e8d3d19b8abd340d83
Claudio Saavedra <csaavedra@igalia.com>
2016-10-11 16:58:58 +0300

    Ship a pkg-config file instead of using CMake to export the library

---
+++ wpe.pc.cmake
@@ -0,0 +1,10 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=${prefix}
+includedir=${prefix}/include
+libdir=${exec_prefix}/lib
+
+Name: wpe
+Description: The wpe library
+Version: @WPE_VERSION@
+Cflags: -I${includedir}
+Libs: -L${libdir} -lWPE
