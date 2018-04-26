b232a3d17728cc7f770f649a555ff54ea3a4ce6e
Michael Catanzaro <mcatanzaro@igalia.com>
2018-04-26 12:57:56 -0500

    Add a dist module, stolen from evolution, and use it
    This requires switching to use the project() command

--- wpe.pc.cmake
+++ wpe.pc.cmake
@@ -5,6 +5,6 @@ libdir=${exec_prefix}/lib
 
 Name: wpe-@WPE_API_VERSION@
 Description: The wpe library
-Version: @WPE_VERSION@
+Version: @PROJECT_VERSION@
 Cflags: -I${includedir}/wpe-@WPE_API_VERSION@
 Libs: -L${libdir} -lWPEBackend-@WPE_API_VERSION@
