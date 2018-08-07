6955316a9c885a0325f3d1adab7062ce3a12810b
Carlos Garcia Campos <cgarcia@igalia.com>
2018-08-07 15:30:44 +0200

    Add xkbcommon as requirement in pkg-config file

--- wpe.pc.cmake
+++ wpe.pc.cmake
@@ -6,5 +6,6 @@ libdir=${exec_prefix}/lib
 Name: wpe-@WPE_API_VERSION@
 Description: The wpe library
 Version: @PROJECT_VERSION@
+Requires: xkbcommon
 Cflags: -I${includedir}/wpe-@WPE_API_VERSION@
 Libs: -L${libdir} -lWPEBackend-@WPE_API_VERSION@
