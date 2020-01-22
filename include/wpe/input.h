f42aa89c1882a9b2d5184f3ca627247642f5289f
Zan Dobersek <zdobersek@igalia.com>
2020-01-22 10:28:04 +0100

    input.h: add wpe_input_axis_2d_event that extnets the base wpe_input_axis_event struct with per-axis delta values.

--- include/wpe/input.h
+++ include/wpe/input.h
@@ -93,6 +93,8 @@ enum wpe_input_axis_event_type {
     wpe_input_axis_event_type_null,
     wpe_input_axis_event_type_motion,
     wpe_input_axis_event_type_motion_smooth,
+
+    wpe_input_axis_event_type_2d = 1 << 16,
 };
 
 struct wpe_input_axis_event {
@@ -103,6 +105,13 @@ struct wpe_input_axis_event {
     uint32_t axis;
     int32_t value;
     uint32_t modifiers;
+};
+
+struct wpe_input_axis_2d_event {
+    struct wpe_input_axis_event base;
+
+    double x_axis;
+    double y_axis;
 };
 
 
