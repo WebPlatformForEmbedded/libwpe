84adbee0a995e00df32090278bc9b72cb53ce217
Adrian Perez de Castro <aperez@igalia.com>
2021-04-14 22:04:15 +0300

    meson: Make scripts/version.py work with older Pythons
    Older Python versions (3.5, 2.x) do not have a __getitem__ method
    on regex match objects, so use the .group() method instead, which is
    available in all versions.

--- scripts/version.py
+++ scripts/version.py
@@ -18,6 +18,6 @@ with open(version_file, "r") as f:
     for line in f.readlines():
         m = version_re.match(line)
         if m:
-            version[m[1]] = m[2]
+            version[m.group(1)] = m.group(2)
 
 print("{}.{}.{}".format(version["MAJOR"], version["MINOR"], version["MICRO"]))
